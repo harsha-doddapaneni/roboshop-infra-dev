cdf61a25d372ffa4ba9274eadb84196f869eb0e9
harsha-doddapaneni <harshadoddapaneni1@gmail.com>
2025-05-08 10:07:39 +0530

    robo

---
+++ 40-eks/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = ">= 5.0.0, < 6.0.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "81s-remot-stat-dev"
+    key    = "roboshop-eks"
+    region = "us-east-1"
+    dynamodb_table = "81s-locking-dev"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
